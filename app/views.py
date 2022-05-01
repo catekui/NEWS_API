8353ab52cd047aadcbef1b2d4728973bf4499041
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-01 22:23:05 +0300

    added routing for index.html

--- app/views.py
+++ app/views.py
@@ -1,11 +1,45 @@
-from flask import render_template
-from app import app
+from flask import Flask,render_template
+# from app import app
+from newsapi import NewsApiClient
 
-#views
+
+app = Flask(__name__)
+
 @app.route('/')
 def index():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    
-    return render_template('index.html')
+
+    newapi = NewsApiClient(api_key="7497b8a3477240c181b4b85f120d9d24") 
+    topheadlines = newapi.get_top_headlines(sources="bbc-news")
+    articles = topheadlines['articles']
+
+
+    news = []
+    description = []
+    link = []
+    image = []
+    time = []
+    content = []
+
+    for i in range(len(articles)):
+        myarticles = articles [i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        link.append(myarticles['url'])
+        image.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles ['content'])
+
+    my_list = zip( news,description,link,image,time,content)
+
+    return render_template('index.html',context=my_list)
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
