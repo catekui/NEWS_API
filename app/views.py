8030e88cc9f7b8521190bd8acfb00a72e90657f1
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-01 23:18:11 +0300

    add health routing

--- app/views.py
+++ app/views.py
@@ -36,7 +36,7 @@ def index():
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html')
-    
+
 # sports route
 @app.route('/sports')
 def sports():
@@ -64,6 +64,33 @@ def sports():
     my_list = zip( news,description,link,image,time,content)
 
     return render_template('sports.html',context=my_list)
+    # health news
+@app.route('/health')
+def health():
+
+    newapi = NewsApiClient(api_key="7497b8a3477240c181b4b85f120d9d24") 
+    topheadlines = newapi.get_top_headlines(sources="News24")
+
+    articles = topheadlines['articles']
+    news = []
+    description = []
+    link = []
+    image = []
+    time = []
+    content = []
+
+    for i in range(len(articles)):
+        myarticles = articles [i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        link.append(myarticles['url'])
+        image.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles ['content'])
+
+    my_list = zip( news,description,link,image,time,content)
+
+    return render_template('health.html',context=my_list)
 
 @app.route('/about')
 def about():
