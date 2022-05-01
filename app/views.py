2e2afa7c4cf0a7553b1175dc8e0edcee812c75fc
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-01 23:26:29 +0300

    added business routing

--- app/views.py
+++ app/views.py
@@ -91,6 +91,33 @@ def health():
     my_list = zip( news,description,link,image,time,content)
 
     return render_template('health.html',context=my_list)
+# business 
+@app.route('/business')
+def business():
+
+    newapi = NewsApiClient(api_key="7497b8a3477240c181b4b85f120d9d24") 
+    topheadlines = newapi.get_top_headlines(sources="CNN")
+
+    articles = topheadlines['articles']
+    news = []
+    description = []
+    link = []
+    image = []
+    time = []
+    content = []
+
+    for i in range(len(articles)):
+        myarticles = articles [i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        link.append(myarticles['url'])
+        image.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles ['content'])
+
+    my_list = zip( news,description,link,image,time,content)
+
+    return render_template('business.html',context=my_list)
 
 @app.route('/about')
 def about():
