6f43d92d3b9d108b61ae05342129035d7a4e78b6
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-05 15:26:35 +0300

    debugging deploying errors

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Documents/NEWS_API/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Documents/NEWS_API/app/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
